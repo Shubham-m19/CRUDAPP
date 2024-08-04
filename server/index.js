5117d6432e8e45304aa3896cdf2172130a585463
Shubham <mhatukadeshubham9462@gmail.com>
2024-08-04 14:19:23 +0530

    starting

---
+++ server/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser")
+const studentRouter = require("./routes/student.route");
+const port = process.env.PORT;
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri).then(() => console.log("connected")).catch((e) => console.log(e));
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use("/api", studentRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);   
+}); 
---
+++ server/models/student.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name : String,
+    age : Number,
+    gender : String
+})
+
+const StudentModel = mongoose.model.insert || mongoose.model("students",studentSchema);
+
+module.exports = StudentModel;
